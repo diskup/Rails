8faffaeea5f5721a2c4bcedd5d12428fccd6aed0
EC2 Default User <ec2-user@ip-172-31-85-132.ec2.internal>
2021-10-11 13:33:37 +0000

    Rails-9

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -5,9 +5,12 @@ class TodolistsController < ApplicationController
   end
 
   def create
-    list=List.new(list_params)
-    list.save
-    redirect_to todolist_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to todolist_path(@list.id)
+    else
+      render :new
+    end
   end
   def index
     @lists = List.all
@@ -15,8 +18,21 @@ class TodolistsController < ApplicationController
   def show
     @list = List.find(params[:id])
   end
+  def edit
+    @list = List.find(params[:id])
+  def update
+    list=List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
+  end
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to todolists_path
+  end
   private
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '2f02ef59abb0899a67a68f6270ad09ad4f307ed1311e5bb813aa7f464b316435a8a658a0886a6e1f1d6ce9cd83e312dff19ad41786e5dbafc3d885f929a3a34c'
